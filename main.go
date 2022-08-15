03ea0e7b8e1de2e7ffc8399e428771d4eec7fb38
Karitham <kar@karitham.dev>
2022-08-15 16:25:40 +0200

    add verbose flag

--- main.go
+++ main.go
@@ -1,45 +1,87 @@
 package main
 
 import (
-	"flag"
 	"io"
 	"io/fs"
-	"log"
+	stdlog "log"
 	"os"
 	"path/filepath"
 	"strconv"
+
+	"github.com/urfave/cli/v2"
 )
 
-var migRoot = flag.String("migrations", "./db/migrations", "migrations directory")
-var upFile = flag.String("up", "up.sql", "up file name")
-
-func init() {
-	flag.Parse()
-
-	if len(os.Args) < 2 {
-		log.Fatal("Usage: database-migrator <out dir>")
-	}
-}
+var log = stdlog.New(os.Stderr, "", 0)
 
 func main() {
-	outdir := os.Args[1]
-	if err := os.MkdirAll(outdir, 0755); err != nil {
-		log.Fatal(err)
+	var migRoot, upFile string
+	var verbose bool
+
+	app := cli.App{
+		Name:  "mig-docker",
+		Usage: "mig-docker <dir>",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "migrations",
+				Value:       "./db/migrations",
+				Destination: &migRoot,
+				Aliases:     []string{"m"},
+			},
+			&cli.StringFlag{
+				Name:        "up",
+				Value:       "up.sql",
+				Destination: &upFile,
+				Aliases:     []string{"u"},
+			},
+			&cli.BoolFlag{
+				Name:        "verbose",
+				Destination: &verbose,
+				Aliases:     []string{"v"},
+			},
+		},
+		Action: func(c *cli.Context) error {
+			outdir := c.Args().First()
+			if outdir == "" {
+				return cli.NewExitError("missing output directory", 1)
+			}
+
+			if err := os.MkdirAll(outdir, 0755); err != nil {
+				log.Fatal(err)
+			}
+
+			fs.WalkDir(os.DirFS(migRoot), ".", func(path string, d fs.DirEntry, err error) error {
+				if err != nil {
+					log.Println(err)
+					return nil
+				}
+
+				if d.Name() != upFile {
+					return nil
+				}
+
+				dir := filepath.Dir(path)
+				if _, err := strconv.Atoi(dir); err != nil {
+					return nil
+				}
+
+				in := filepath.Join(migRoot, path)
+				out := filepath.Join(outdir, dir+".sql")
+				if err := CopyFile(in, out); err != nil {
+					return err
+				}
+
+				if verbose {
+					log.Printf("%s -> %s", in, out)
+				}
+				return nil
+			})
+			return nil
+		},
 	}
 
-	fs.WalkDir(os.DirFS(*migRoot), ".", func(path string, d fs.DirEntry, _ error) error {
-		if d.Name() != *upFile {
-			return nil
-		}
-
-		dir := filepath.Dir(path)
-		if _, err := strconv.Atoi(dir); err != nil {
-			return nil
-		}
-
-		CopyFile(filepath.Join(*migRoot, path), filepath.Join(outdir, dir+".sql"))
-		return nil
-	})
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
 
 // CopyFile copies a file from src to dst.
